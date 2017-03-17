4c16007b60521a16218f935c85ca505e21b9077e
Jen Kagan <kaganjd@gmail.com>
2017-03-16 22:39:04 -0400

    cleanup, integrating with tabletop.js

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Databasing the Homeless
-This is a sketch of what could be a larger project about data and databases, knowledge production, and the electronic turn in social services. It's inspired by Craig Willse's 2016 book, "The Value of Homelessness: Managing Surplus Life in the United States"—specifically, the "Governing through Numbers" chapter.
+This is a WIP sketch of what could be a larger project about data and databases, knowledge production, and the electronic turn in social services. It's inspired by Craig Willse's 2016 book, "The Value of Homelessness: Managing Surplus Life in the United States"—specifically, the "Governing through Numbers" chapter.
 
-At this point, there is only one modal. When you click the "First Name" input, you see part of an interview from the Signs for the Homeless project (link below). I'd like to create modals for every input box.
+The text is clipped from interviews done as part of a 2011-2013 project called "Signs for the Homeless". It's a problematic source, and I'm interested in making another version with clips from Joe Bernstein's 2016 article in the Seattle Weekly, "Stealing From the Homeless".
 
 In disrupting the data field this way, the idea is to show how databases constrain the kinds of information we can know about a person. The resulting "statistical portraits," to use Willse's phrase, stand in for a population that doesn't actually exist until the database brings it into being. This reality has important applications for how we use databases (and algorithms that rely on them) to create and inform policy. 
 
@@ -10,9 +10,11 @@ In disrupting the data field this way, the idea is to show how databases constra
 
 [HMIS training manual](http://www.sbcounty.gov/capsbc/docs/HMIS/CAPSBC%20HMIS%20Manual%204%203%203V1%201.pdf)
 
-[“Andres Serrano teaches us how not to help the homeless”](http://hyperallergic.com/101311/andres-serrano-shows-us-how-not-to-help-the-homeless/) by Jillian Steinhauer
+"Signs for the Homeless" project is no longer online, but here's [an article](http://www.bostonmagazine.com/arts-entertainment/blog/2013/08/07/signs-for-the-homeless-tumblr-kenji-nakayama/) about it
 
-[Signs for the Homeless](http://homelesssigns.tumblr.com/)
+["Stealing From the Homeless"](http://www.seattleweekly.com/news/stealing-from-the-homeless/) by Joe Bernstein
+
+[“Andres Serrano teaches us how not to help the homeless”](http://hyperallergic.com/101311/andres-serrano-shows-us-how-not-to-help-the-homeless/) by Jillian Steinhauer
 
 [2015 Continuum of Care data report, NYC](https://www.hudexchange.info/resource/reportmanagement/published/CoC_PopSub_CoC_NY-600-2015_NY_2015.pdf)
 
--- databasing.js
+++ databasing.js
@@ -1,39 +1,76 @@
-var modal1;
+var rawData;
+var modals = [];
+var picker;
+
+function init() {
+  // tabletop.js init() function returns data as an array of objects, like this:
+  // [{"animal": "horse",
+  //  "color": "brown"},
+  //  {"animal": "chick",
+  //  "color": "yellow"}]
+Tabletop.init( { key: '1asZC5tWkBpMZ47S05VxJj0fh3gyMjaKYpK1sDNE2iCA',
+                   callback: gotData,
+                   simpleSheet: true } )
+}
+window.addEventListener('DOMContentLoaded', init)
+
+function gotData(data, tabletop) {
+  rawData = data;
+  console.log("raw data: ", rawData)
+  createModal();
+}
+
 function createModal() {
-	modal1 = document.getElementById('myModal1');
-	}
-createModal();
-
-// Get the button that opens the modal
-// When the user clicks the button, open the modal 
-function openModal() {
-var i;
-var btn = document.getElementsByTagName("input");
-for (i = 0; i < btn.length; i++) {
-btn = document.getElementsByTagName("input")[i]; //loop doesn't work :(
-btn.onclick = function() {
-    modal1.style.display = "block";
-	}
-}
-}
-openModal();
-
-
-// Get the <span> element that closes the modal
-// When the user clicks on <span> (x), close the modal
-// When the user clicks anywhere outside of the modal, close it
-function closeModal() {
-	
-var span = document.getElementsByClassName("close")[0];
-
-span.onclick = function() {
-    modal1.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal1) {
-        modal1.style.display = "none";
+  var modalName;
+  var modalText;
+  var parentDiv;
+  // if there's data
+  if (rawData.length) {
+    // iterate over each object in the data set
+    for (var i = 0; i < rawData.length; i++) {
+      // assign each object a modalName
+      modalName = 'modal' + i;
+      // push the modal name to the modals array
+      modals.push(modalName);
+      // get the modal text from the "text2" column of the spreadsheet
+      modalText = rawData[i]["text2"];
+      // set the parent element to the <p> tags
+      parentEl = document.getElementById(modals[i]).children[0].children[1]
+      parentEl.innerText = modalText;
+      console.log(parentEl);
     }
-	}
+  }
+  openModal();
 }
-closeModal();
 
+function openModal() {
+  var inputs = document.getElementsByTagName("input");
+  var btn = [];
+  picker = Math.floor(Math.random() * modals.length);
+  console.log(picker);
+  for (var i = 0; i < inputs.length; i++) {
+    btn = inputs[i]
+    btn.onclick = function() {
+      document.getElementById(modals[picker]).style.display = "block";
+    }
+  }
+}
+
+function closeModal() {
+  var span = document.getElementsByClassName("close")[picker];
+  // if the user clicks the 'x', close it
+  span.onclick = function() {
+    console.log("picker: ", picker)
+    console.log("modals[picker]: ", modals[picker])
+    document.getElementById(modals[picker]).style.display = "none";
+  }
+  // if the user clicks inside the modal, close it
+  window.onclick = function(event) {
+    if (event.target == document.getElementById(modals[picker])) {
+      document.getElementById(modals[picker]).style.display = "none";
+    }
+  }
+  // TODO: how do i reset the picker value?
+}
+
+window.setTimeout(closeModal, 3000);
